8c02f1d24b31f70908265b56ccb54ec84fa176e6
Ophélie Adams <ophelieadams@MacBook-Pro-de-Ophelie.local>
2023-04-05 17:26:28 +0200

    remove behat demo files

--- features/browse_shop.feature
+++ features/browse_shop.feature
@@ -1,10 +1,15 @@
-@browse @amazon
+@browse
 Feature: Browse the Amazon online shop
     In order to find products
     As a script
     I want to get informations about products
 
-    @france
-    Scenario: It receives a response from Amazon shop
-        When a demo scenario sends a request to "https://www.amazon.fr"
+    Scenario Outline: It receives a response from "<online shop>" shop
+        Given a shop named "<online shop>" with a website "<url>"
         Then the response should be received
+
+        Examples:
+            | online shop | url |
+            | Amazon      | https://www.amazon.fr           |
+            | Rakuten     | https://fr.shopping.rakuten.com |
+            | Darty       | https://www.darty.com           |
--- tests/Behat/BrowseShopContext.php
+++ tests/Behat/BrowseShopContext.php
@@ -4,9 +4,62 @@ declare(strict_types=1);
 
 namespace App\Tests\Behat;
 
+use App\Entity\Shop;
+use App\Entity\ShopUrl;
 use Behat\Behat\Context\Context;
+use Symfony\Component\BrowserKit\HttpBrowser;
 
-final class BrowseShopAmazon implements Context
+use Symfony\Component\DomCrawler\Crawler;
+use Symfony\Component\HttpClient\HttpClient;
+use function PHPUnit\Framework\assertSame;
+
+final class BrowseShopContext implements Context
 {
+    /**
+     * @var HttpBrowser
+     */
+    private HttpBrowser $browser;
 
+    /**
+     * @var Crawler
+     */
+    private Crawler $response;
+
+    public function __construct()
+    {
+        $this->browser = new HttpBrowser(HttpClient::create());
+    }
+
+    /**
+     * @Given a shop named :shopName with a website :url
+     */
+    public function aShopNamedWithAWebsite($shopName, $url)
+    {
+        $expectedShops = [
+            "Amazon"=>"https://www.amazon.fr",
+            "Rakuten"=>"https://fr.shopping.rakuten.com",
+            "Darty"=>"https://www.darty.com"
+        ];
+
+        assertSame($expectedShops[$shopName], $url);
+
+        $shop = new Shop();
+        $shop->setName($shopName);
+
+        $shopUrl = new ShopUrl();
+        $shopUrl->setShop($shop);
+        $shopUrl->setUrl($url);
+
+        $this->response = $this->browser->request('GET', $url);
+    }
+
+    /**
+     * @Then the response should be received
+     */
+    public function theResponseShouldBeReceived(): void
+    {
+        if ($this->response === null) {
+            throw new \HttpRuntimeException('No response received');
+        }
+    }
 }
